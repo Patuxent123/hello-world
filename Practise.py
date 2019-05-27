d6d01564b0a202bae887f36e69dccd32a2455d34
Patuxent123 <50549922+Patuxent123@users.noreply.github.com>
2019-05-26 21:02:23 -0400

    Add files via upload

---
+++ Practise.py
@@ -0,0 +1,46 @@
+
+mylist={1,2,3,4}
+myDic={"key":"value", "key2":"value"}
+
+myPhones={"Iphone X":1000, "Sumsang":"900"}
+
+print(myPhones)
+
+Iphone_Price=myPhones["Iphone X"]
+
+print("Iphone price: "+str(Iphone_Price))
+
+# change key value
+
+myPhones["Iphone X"]=500
+
+print(myPhones)
+
+myPhones.clear()
+
+print(myPhones)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
